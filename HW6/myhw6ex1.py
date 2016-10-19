54cd9a38d44e4bb3dcf3a69c2dd1613fc2fea872
Sheng Zhang <shengzhang0830@hotmail.com>
2016-10-19 13:43:30 -0400

    myhw6mod.py and myhw6ex1.py created.

---
+++ HW6/myhw6ex1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import myhw6mod as mymod
+from sys import argv
+
+if len(argv) != 2:
+	print("You must enter one integer argument.")
+else:
+	user_input = argv[1]	
+	check = 0
+	while(check!=1):
+	    try:
+	    	x = int(user_input)
+	    except:
+	    	print("You must enter one integer argument.")
+	     	user_input = input("Please enter a valid input.")
+	    else:
+	    	check = 1
+	    	break
+
+	mymod.hwfunone(x)
+
+
---
+++ HW6/myhw6mod.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def hwfunone(n):
+	for i in range(1,n+1):
+		for j in range (1,n+1):
+			print(i,"+",j,"=",(i+j))
+	return()
+
+
