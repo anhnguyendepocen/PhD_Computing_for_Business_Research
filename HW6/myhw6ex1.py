268a37188b7d9e971fe89ce752603f188dfc224d
Sheng Zhang <shengzhang0830@hotmail.com>
2016-10-20 17:06:43 -0400

    fixed an error for question 1, PlotTaxiData.py created.

--- HW6/myhw6ex1.py
+++ HW6/myhw6ex1.py
@@ -6,20 +6,18 @@ from sys import argv
 if len(argv) != 2:
 	print("You must enter one integer argument.")
 else:
-	user_input = argv[1]	
+	user_input = argv[1]
 	check = 0
-	while(check!=1):
-	    try:
-	    	x = int(user_input)
-	    except:
-	    	print("You must enter one integer argument.")
-	     	user_input = input("Please enter a valid input.")
-	    else:
-	    	check = 1
-	    	break
-
+	while(check != 1):
+		try:
+			x = int(user_input)
+		except:
+			print("You must enter one integer argument.")
+			user_input = input("Please enter a valid input: ")
+		else:
+			check = 1
+			break
 	mymod.hwfunone(x)
 
 
-# used to run but now doesn't work
 # should we give the user another chance if the length is not 2?
